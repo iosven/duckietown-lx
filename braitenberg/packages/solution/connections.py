c10ce3c9bee62e8f949f60fdaa6bce870f297c70
aisven <aisven822@gmail.com>
2022-12-16 03:59:15 +0100

    Duckietown MOOC lx22 BV1 aisven 6022 solution second commit

--- braitenberg/packages/solution/connections.py
+++ braitenberg/packages/solution/connections.py
@@ -9,34 +9,44 @@ def get_motor_left_matrix(shape: Tuple[int, int]) -> np.ndarray:
     image_height = 480
     image_width = 640
 
-    #middle_index_vertical_exclusive = 240
-    middle_index_horizontal_exclusive = 320
+    # middle_index_vertical = 240
+    # middle_index_horizontal = 320
+    # minimal bias to steering right by using a smaller blind gap in the middle of the optical perceptor
+    middle_index_horizontal_gap_start = 302
+    middle_index_horizontal_gap_end = 338
+
+    # relevant_vertical_speed_source_to_index = 190
 
     relevant_vertical_from_index = 200
-    relevant_vertical_to_index_exclusive = 400
-    relevant_horizontal_from_index = 100
-    relevant_horizontal_to_index_exclusive = 540
+    relevant_vertical_to_index = 400
+    relevant_horizontal_from_index = 95
+    relevant_horizontal_to_index = 545
 
-    #relevant_bar_vertical = (relevant_vertical_from_index, relevant_vertical_to_index_exclusive)
-    #relevant_bar_horizon = (relevant_horizontal_from_index, relevant_horizontal_to_index_exclusive)
+    # relevant_bar_vertical = (relevant_vertical_from_index, relevant_vertical_to_index)
+    # relevant_bar_horizon = (relevant_horizontal_from_index, relevant_horizontal_to_index)
+
+    # more bias for steering right
+    steering_right_bias_magic_number = 0.15
 
     # if there is a duck on the left, the left motor needs to be faster in order to turn right
-    res[relevant_vertical_from_index:relevant_vertical_to_index_exclusive, relevant_horizontal_from_index:middle_index_horizontal_exclusive] = 0.75
+    res[relevant_vertical_from_index:relevant_vertical_to_index, relevant_horizontal_from_index:middle_index_horizontal_gap_start] = (0.8 + steering_right_bias_magic_number)
 
     # if there is a duck on the right, the left motor needs to be slower in order to turn left
-    res[relevant_vertical_from_index:relevant_vertical_to_index_exclusive, middle_index_horizontal_exclusive:image_width] = 0.25
+    res[relevant_vertical_from_index:relevant_vertical_to_index, middle_index_horizontal_gap_end:image_width] = 0.04
 
     # area above considered irrelevant
     res[0:relevant_vertical_from_index, :] = 0.0
+    # tried to use some far away duckies on the horizon as source of speed
+    # res[0:relevant_vertical_speed_source_to_index, :] = 0.02
 
     # area below considered irrelevant
-    res[relevant_vertical_to_index_exclusive:image_height, :] = 0.0
+    res[relevant_vertical_to_index:image_height, :] = 0.0
 
-    # area on the left considered irrelevant
+    # area on the far left considered irrelevant
     res[:, 0:relevant_horizontal_from_index] = 0.0
 
-    # area on the right considered irrelevant
-    res[:, relevant_horizontal_to_index_exclusive:image_width] = 0.0
+    # area on the far right considered irrelevant
+    res[:, relevant_horizontal_to_index:image_width] = 0.0
 
     return res
 
@@ -47,33 +57,39 @@ def get_motor_right_matrix(shape: Tuple[int, int]) -> np.ndarray:
     image_height = 480
     image_width = 640
 
-    #middle_index_vertical_exclusive = 240
-    middle_index_horizontal_exclusive = 320
+    # middle_index_vertical = 240
+    # middle_index_horizontal = 320
+    middle_index_horizontal_gap_start = 295
+    middle_index_horizontal_gap_end = 354
+
+    # relevant_vertical_speed_source_to_index = 190
 
     relevant_vertical_from_index = 200
-    relevant_vertical_to_index_exclusive = 400
-    relevant_horizontal_from_index = 100
-    relevant_horizontal_to_index_exclusive = 540
+    relevant_vertical_to_index = 400
+    relevant_horizontal_from_index = 95
+    relevant_horizontal_to_index = 545
 
-    #relevant_bar_vertical = (relevant_vertical_from_index, relevant_vertical_to_index_exclusive)
-    #relevant_bar_horizon = (relevant_horizontal_from_index, relevant_horizontal_to_index_exclusive)
+    # relevant_bar_vertical = (relevant_vertical_from_index, relevant_vertical_to_index)
+    # relevant_bar_horizon = (relevant_horizontal_from_index, relevant_horizontal_to_index)
 
     # if there is a duck on the left, the right motor needs to be slower
-    res[relevant_vertical_from_index:relevant_vertical_to_index_exclusive, relevant_horizontal_from_index:middle_index_horizontal_exclusive] = 0.25
+    res[relevant_vertical_from_index:relevant_vertical_to_index, relevant_horizontal_from_index:middle_index_horizontal_gap_start] = 0.04
 
     # if there is a duck on the right, the right motor needs to be faster
-    res[relevant_vertical_from_index:relevant_vertical_to_index_exclusive, middle_index_horizontal_exclusive:image_width] = 0.75
+    res[relevant_vertical_from_index:relevant_vertical_to_index, middle_index_horizontal_gap_end:image_width] = 0.8
 
     # area above considered irrelevant
     res[0:relevant_vertical_from_index, :] = 0.0
+    # tried to use some far away duckies on the horizon as source of speed
+    # res[0:relevant_vertical_speed_source_to_index, :] = 0.02
 
     # area below considered irrelevant
-    res[relevant_vertical_to_index_exclusive:image_height, :] = 0.0
+    res[relevant_vertical_to_index:image_height, :] = 0.0
 
-    # area on the left considered irrelevant
+    # area on the far left considered irrelevant
     res[:, 0:relevant_horizontal_from_index] = 0.0
 
-    # area on the right considered irrelevant
-    res[:, relevant_horizontal_to_index_exclusive:image_width] = 0.0
+    # area on the far right considered irrelevant
+    res[:, relevant_horizontal_to_index:image_width] = 0.0
 
     return res
