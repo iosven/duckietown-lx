8c176f6391030bcc1713374fb2f60543c26601aa
aisven <aisven822@gmail.com>
2022-12-23 23:00:31 +0100

    Duckietown MOOC lx22 BV1 aisven 6022 solution third commit

--- braitenberg/packages/solution/connections.py
+++ braitenberg/packages/solution/connections.py
@@ -11,41 +11,34 @@ def get_motor_left_matrix(shape: Tuple[int, int]) -> np.ndarray:
 
     # middle_index_vertical = 240
     # middle_index_horizontal = 320
-    # minimal bias to steering right by using a smaller blind gap in the middle of the optical perceptor
-    middle_index_horizontal_gap_start = 302
-    middle_index_horizontal_gap_end = 338
 
-    # relevant_vertical_speed_source_to_index = 190
+    # we have zeros in the middle in res[:,315:325]
+    # for corner cases when a duck is in front but mostly on one side
+    middle_index_horizontal_gap_start = 315
+    middle_index_horizontal_gap_end = 325
 
-    relevant_vertical_from_index = 200
-    relevant_vertical_to_index = 400
-    relevant_horizontal_from_index = 95
-    relevant_horizontal_to_index = 545
+    # we have zeros above the horizon in res[0:80,:] to ignore things in sky, walls, background
+    relevant_vertical_from_index = 10
+    # we have zeros where we see the floor in res[400:480,:] to ignore markings on the floor
+    relevant_vertical_to_index = 470
+    # we have zeros on the far left res[:,0:80] to ignore things far left
+    relevant_horizontal_from_index = 10
+    # we have zeros on the far right res[:,560:640] to ignore things far right
+    relevant_horizontal_to_index = 630
 
-    # relevant_bar_vertical = (relevant_vertical_from_index, relevant_vertical_to_index)
-    # relevant_bar_horizon = (relevant_horizontal_from_index, relevant_horizontal_to_index)
-
-    # more bias for steering right
-    steering_right_bias_magic_number = 0.15
+    # bias for steering right
+    steering_right_bias_magic_number = 0.5
 
     # if there is a duck on the left, the left motor needs to be faster in order to turn right
-    res[relevant_vertical_from_index:relevant_vertical_to_index, relevant_horizontal_from_index:middle_index_horizontal_gap_start] = (0.8 + steering_right_bias_magic_number)
+    res[relevant_vertical_from_index:relevant_vertical_to_index, relevant_horizontal_from_index:middle_index_horizontal_gap_start] = (0.95 + steering_right_bias_magic_number)
 
     # if there is a duck on the right, the left motor needs to be slower in order to turn left
-    res[relevant_vertical_from_index:relevant_vertical_to_index, middle_index_horizontal_gap_end:image_width] = 0.04
+    res[relevant_vertical_from_index:relevant_vertical_to_index, middle_index_horizontal_gap_end:image_width] = -0.012
 
-    # area above considered irrelevant
+    # just in case set the areas we want to ignore to zero explicitly
     res[0:relevant_vertical_from_index, :] = 0.0
-    # tried to use some far away duckies on the horizon as source of speed
-    # res[0:relevant_vertical_speed_source_to_index, :] = 0.02
-
-    # area below considered irrelevant
     res[relevant_vertical_to_index:image_height, :] = 0.0
-
-    # area on the far left considered irrelevant
     res[:, 0:relevant_horizontal_from_index] = 0.0
-
-    # area on the far right considered irrelevant
     res[:, relevant_horizontal_to_index:image_width] = 0.0
 
     return res
@@ -59,37 +52,31 @@ def get_motor_right_matrix(shape: Tuple[int, int]) -> np.ndarray:
 
     # middle_index_vertical = 240
     # middle_index_horizontal = 320
-    middle_index_horizontal_gap_start = 295
-    middle_index_horizontal_gap_end = 354
 
-    # relevant_vertical_speed_source_to_index = 190
+    # we have zeros in the middle in res[:,315:325]
+    # for corner cases when a duck is in front but mostly on one side
+    middle_index_horizontal_gap_start = 315
+    middle_index_horizontal_gap_end = 325
 
-    relevant_vertical_from_index = 200
-    relevant_vertical_to_index = 400
-    relevant_horizontal_from_index = 95
-    relevant_horizontal_to_index = 545
+    # we have zeros above the horizon to ignore some things in sky, walls, background
+    relevant_vertical_from_index = 10
+    # we have zeros where we see the floor
+    relevant_vertical_to_index = 470
+    # we have zeros on the far left to ignore things far left
+    relevant_horizontal_from_index = 10
+    # we have zeros on the far right to ignore things far right
+    relevant_horizontal_to_index = 630
 
-    # relevant_bar_vertical = (relevant_vertical_from_index, relevant_vertical_to_index)
-    # relevant_bar_horizon = (relevant_horizontal_from_index, relevant_horizontal_to_index)
+    # if there is a duck on the left, the left motor needs to be faster in order to turn right
+    res[relevant_vertical_from_index:relevant_vertical_to_index, relevant_horizontal_from_index:middle_index_horizontal_gap_start] = -0.015
 
-    # if there is a duck on the left, the right motor needs to be slower
-    res[relevant_vertical_from_index:relevant_vertical_to_index, relevant_horizontal_from_index:middle_index_horizontal_gap_start] = 0.04
+    # if there is a duck on the right, the left motor needs to be slower in order to turn left
+    res[relevant_vertical_from_index:relevant_vertical_to_index, middle_index_horizontal_gap_end:image_width] = 0.95
 
-    # if there is a duck on the right, the right motor needs to be faster
-    res[relevant_vertical_from_index:relevant_vertical_to_index, middle_index_horizontal_gap_end:image_width] = 0.8
-
-    # area above considered irrelevant
+    # just in case set the areas we want to ignore to zero explicitly
     res[0:relevant_vertical_from_index, :] = 0.0
-    # tried to use some far away duckies on the horizon as source of speed
-    # res[0:relevant_vertical_speed_source_to_index, :] = 0.02
-
-    # area below considered irrelevant
     res[relevant_vertical_to_index:image_height, :] = 0.0
-
-    # area on the far left considered irrelevant
     res[:, 0:relevant_horizontal_from_index] = 0.0
-
-    # area on the far right considered irrelevant
     res[:, relevant_horizontal_to_index:image_width] = 0.0
 
     return res
--- braitenberg/packages/solution/preprocessing.py
+++ braitenberg/packages/solution/preprocessing.py
@@ -1,9 +1,8 @@
 import cv2
 import numpy as np
 
-lower_hsv = np.array([000, 42, 82])
-upper_hsv = np.array([15, 255, 255])
-
+lower_hsv = np.array([0, 10, 70])
+upper_hsv = np.array([95, 255, 255])
 
 def preprocess(image_rgb: np.ndarray) -> np.ndarray:
     """Returns a 2D array"""
